f06459882d2546517091e818809f54e9848ff45f
Luigi Santagada <luigi.santagada@live.com>
2022-12-28 13:47:05 +0100

    .

--- laserSensorDistance/laserSensorDistance.ino
+++ laserSensorDistance/laserSensorDistance.ino
@@ -19,7 +19,9 @@ The range readings are in units of mm. */
 
 #include <Wire.h>
 #include <VL53L0X.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address, if it's not working try 0x27.
 VL53L0X sensor;
 
 void setup()
@@ -40,15 +42,20 @@ void setup()
     /*sensor.startContinuous();*/
     sensor.setMeasurementTimingBudget(20000);
     sensor.startContinuous();
+	lcd.begin(16, 1);   // iInit the LCD for 16 chars 2 lines
 }
 
 void loop()
 {
 	int distance = sensor.readRangeContinuousMillimeters();
 	//distance = distance - 55;// -55 is to compensate for error. Change or set it to zero to make it work for your sensor
-	Serial.print("Distance: ");
+	/*Serial.print("Distance: ");
 	Serial.print(distance);
-	Serial.print("mm");
+	Serial.print("mm");*/
+	String a = "distance : ";
+	a.concat(distance);
+	a.concat(" mm");
+	lcdPrintMessage(a);
 	if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 	Serial.println();
 	if (distance < 100 && distance > 10)
@@ -60,4 +67,24 @@ void loop()
 	{
 		digitalWrite(9, LOW);
 	}
+}
+void lcdPrintMessage(String message)
+{
+	lcd.backlight();   // Turn on the backligt (try lcd.noBaklight() to turn it off)
+	lcd.setCursor(0, 1); //First line
+	lcd.println(message);
+}
+
+void lcdSlideMessage(String message) {
+	lcd.begin(16, 1);   // iInit the LCD for 16 chars 2 lines
+	lcd.backlight();   // Turn on the backligt (try lcd.noBaklight() to turn it off)
+	lcd.setCursor(16, 1); //First line
+	lcd.scrollDisplayLeft();
+	lcd.autoscroll();
+	for (int thisChar = 0; thisChar < message.length(); thisChar++) {
+		lcd.print(message.charAt(thisChar));
+		delay(350);
+	}
+	delay(2000);
+	lcd.clear();
 }
